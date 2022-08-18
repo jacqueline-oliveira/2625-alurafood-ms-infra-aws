10b2b993952f29e564b842efc02aa317277c8364
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-08-18 12:03:23 -0300

    video 5.2 - inclusão do Log Group

--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -1,14 +1,18 @@
 package com.myorg;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 import java.util.HashMap;
@@ -33,15 +37,22 @@ public class AluraServiceStack extends Stack {
                 .serviceName("alura-service-ola")
                 .cluster(cluster)           // Required
                 .cpu(512)                   // Default is 256
-                .desiredCount(1)            // Default is 1
+                .desiredCount(3)            // Default is 1
                 .listenerPort(8080)
                 .assignPublicIp(true)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
                                 .image(ContainerImage.fromEcrRepository(iRepository))
                                 .containerPort(8080)
-                                .environment(autenticacao)
                                 .containerName("app_ola")
+                                .environment(autenticacao)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "PedidosMsLogGroup")
+                                                .logGroupName("PedidosMsLog")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("PedidosMS")
+                                        .build()))
                                 .build())
                 .memoryLimitMiB(1024)       // Default is 512
                 .publicLoadBalancer(true)   // Default is false
